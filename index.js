4847ed314114d02853d11c57ffda7016a8201e45
Beby Kurniawan <bebykurniawan2006@gmail.com>
2023-10-27 20:04:49 +0700

    kasus - 1

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ btn.addEventListener('click',function() {
     document.body.classList.toggle('biru')
 }
 )
+
+
+const btnWarna = document.createElement('button')
+const textBtn = document.createTextNode("Ubah Warna Random")
+btnWarna.appendChild(textBtn)
+btnWarna.setAttribute('type','button')
+btn.after(btnWarna)
+
+btnWarna.addEventListener('click',function() {
+    const r = Math.round(Math.random() * 255 + 1)
+    const g = Math.round(Math.random() * 255 + 1)
+    const b = Math.round(Math.random() * 255 + 1)
+    document.body.style.backgroundColor = "rgb("+ r +","+ g +","+ b +")";
+})
